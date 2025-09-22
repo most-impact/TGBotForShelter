cde77e96cc2d074849ff056ee5a91e39689b3fef
Dmitriy Kurilko <dimakurilko0606@mail.ru>
2025-09-22 13:23:50 +0300

    Initial commit

---
+++ src/main/java/pro/dev/TGBotForShelter/TgBotForShelterApplication.java
@@ -0,0 +1,13 @@
+package pro.dev.TGBotForShelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBotForShelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgBotForShelterApplication.class, args);
+	}
+
+}
